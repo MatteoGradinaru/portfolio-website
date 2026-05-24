7813e5a6997e4b752603e5615e7c39c50325414d
Matteo Gradinaru <matteo.gradinaru@student.ucll.be>
2026-05-24 14:34:56 +0200

    build: made the file structure and created package.json

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <div
+          style={{
+            backgroundColor: "white",
+            color: "black",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontFamily: "sans-serif",
+          }}
+        >
+          <h1>My Portfolio Website</h1>
+        </div>
+      </body>
+    </html>
+  );
+}
